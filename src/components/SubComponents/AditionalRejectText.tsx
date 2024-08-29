99b9cff76105f1c8e5d758483c734eabd7b68b6e
secretindex <caio.smelo04@gmail.com>
2024-08-29 17:10:48 -0300

    menu does not trespass bottom boundary

--- src/components/SubComponents/AditionalRejectText.tsx
+++ src/components/SubComponents/AditionalRejectText.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useEffect, useRef } from "react"
 
 import { List } from "antd"
 import AditionalTexts from "../objects/AditionalRejectTexts"
@@ -11,8 +11,34 @@ interface RejectProps {
 }
 
 const AditionalRejectText: FC<RejectProps> = ({ mouseCoords }) => {
+  const menuRef = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    const menu = menuRef.current
+
+    console.log(menu)
+
+    if (menu) {
+      const bound = menu.getBoundingClientRect()
+
+      console.log(bound.bottom  + " " + window.innerHeight)
+
+      if (bound.bottom > window.innerHeight) {
+        menu.style.top = `${Math.abs(mouseCoords.y - (bound.bottom - window.innerHeight))}px`;
+      }
+    }
+  }, [mouseCoords])
+
   return (
-    <div style={{ zIndex: 1000, position: "absolute", top: mouseCoords.y, left: mouseCoords.x }} className="dropdown flex flex-col justify-between gap-4 overflow-auto bg-[#fefefe]" >
+    <div
+      ref={menuRef}
+      style={{
+        zIndex: 1000,
+        position: "absolute",
+        top: mouseCoords.y,
+        left: mouseCoords.x,
+      }}
+      className="dropdown flex flex-col justify-between gap-4 overflow-auto bg-[#fefefe]"
+    >
       <header className="mt-2">
         <h4 className="text-center font-bold">Textos adicionais</h4>
       </header>
