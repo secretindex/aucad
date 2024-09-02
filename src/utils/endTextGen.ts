c13887da61030e0d2779354560a9923cba280493
secretindex <caio.smelo04@gmail.com>
2024-09-02 14:36:53 -0300

    a little refactoring

--- src/utils/endTextGen.ts
+++ src/utils/endTextGen.ts
@@ -17,70 +17,29 @@ import {
   standardPensioner,
   PensionerCheck,
 } from "./documents and models/pensionerDocuments"
+
 import { inactivesStandard } from "./documents and models/inactivesDocuments"
+
+type FieldsType =
+  | RequiredDocs
+  | FinalTextDocuments
+  | PensionerDocuments
+  | PensionerCheck
+  | InactivesDocuments
 
 class EndText {
   private upper: string = text.upperText
   private midText: string = ""
   private bottom: string = text.bottomText
-  private textFields: Array<string>
-  private supportNumber: string
-  fields:
-    | RequiredDocs
-    | FinalTextDocuments
-    | PensionerDocuments
-    | PensionerCheck
-    | InactivesDocuments
+  protected textFields: Array<string>
+  fields: FieldsType
 
-  constructor(fields: RequiredDocs | FinalTextDocuments | PensionerDocuments | InactivesDocuments) {
+  constructor(fields: FieldsType) {
     this.fields = fields
     this.textFields = []
-    this.supportNumber =
-      "\nNúmero do suporte: +55 (96) 3082-1277\nHorários: 08:00 às 17:00, segunda a sexta\n"
   }
 
-  addTextFields(): Array<string> | [] {
-    for (const i in this.fields) {
-      if ((this.fields as RequiredDocs)[i as keyof RequiredDocs] === true) {
-        this.textFields.push(i)
-      }
-    }
-
-    return []
-  }
-
-  returnActivesRejectText(): any {
-    let support: string = ""
-
-    for (const i in this.fields) {
-      if (
-        typeof (this.fields as RequiredDocs)[i as keyof RequiredDocs] !==
-        "string"
-      ) {
-        this.textFields.push(
-          (this.fields as RequiredDocs)[i as keyof RequiredDocs] === true
-            ? ""
-            : standardDocuments[i as keyof StandardDocuments]
-        )
-      } else {
-        let supportValue = (this.fields as FinalTextDocuments)[
-          i as keyof FinalTextDocuments
-        ]
-        console.log("this is support value " + supportValue)
-        support =
-          supportValue === "dep/id/10" || supportValue === "id/10"
-            ? this.supportNumber
-            : ""
-        this.textFields.push(
-          invalidDocuments[
-            (this.fields as FinalTextDocuments)[
-              i as keyof FinalTextDocuments
-            ] as keyof InvalidDocuments
-          ]
-        )
-      }
-    }
-
+  returnText(): string {
     for (let i = 0; i < this.textFields.length; i++) {
       if (
         typeof this.textFields[i] === "string" &&
@@ -93,15 +52,38 @@ class EndText {
     }
 
     if (this.midText) {
-      return this.upper + "\n" + this.midText + support + this.bottom
+      return this.upper + "\n" + this.midText + this.bottom
     } else {
       return "Cadastro completo 👍"
     }
   }
 
-  returnInactiveText() {
-    let support: string = ""
+  returnActivesRejectText(): string {
+    for (const i in this.fields) {
+      if (
+        typeof (this.fields as RequiredDocs)[i as keyof RequiredDocs] !==
+        "string"
+      ) {
+        this.textFields.push(
+          (this.fields as RequiredDocs)[i as keyof RequiredDocs] === true
+            ? ""
+            : standardDocuments[i as keyof StandardDocuments]
+        )
+      } else {
+        this.textFields.push(
+          invalidDocuments[
+            (this.fields as FinalTextDocuments)[
+              i as keyof FinalTextDocuments
+            ] as keyof InvalidDocuments
+          ]
+        )
+      }
+    }
 
+    return this.returnText()
+  }
+
+  returnInactiveText(): string {
     for (const i in this.fields) {
       if (
         typeof (this.fields as InactivesDocuments)[
@@ -115,14 +97,6 @@ class EndText {
             : inactivesStandard[i as keyof InactiveStandard]
         )
       } else {
-        let supportValue = (this.fields as InactivesDocuments)[
-          i as keyof InactivesDocuments
-        ]
-        console.log("this is support value " + supportValue)
-        support =
-          supportValue === "dep/id/10" || supportValue === "id/10"
-            ? this.supportNumber
-            : ""
         this.textFields.push(
           invalidDocuments[
             (this.fields as InactivesDocuments)[
@@ -133,27 +107,10 @@ class EndText {
       }
     }
 
-    for (let i = 0; i < this.textFields.length; i++) {
-      if (
-        typeof this.textFields[i] === "string" &&
-        this.textFields[i].length === 0
-      )
-        continue
-      if (typeof this.textFields[i] === "undefined") continue
-
-      if (this.textFields[i]) this.midText += `- ${this.textFields[i]};\n`
-    }
-
-    if (this.midText) {
-      return this.upper + "\n" + this.midText + support + this.bottom
-    } else {
-      return "Cadastro completo 👍"
-    }
+    return this.returnText()
   }
 
-  returnPensionerText() {
-    let support: string = ""
-
+  returnPensionerText(): string {
     for (const i in this.fields) {
       if (
         typeof (this.fields as PensionerDocuments)[
@@ -166,11 +123,6 @@ class EndText {
             : standardPensioner[i as keyof PensionerCheck]
         )
       } else {
-        let supportValue = (this.fields as PensionerDocuments)[
-          i as keyof PensionerDocuments
-        ]
-        console.log("this is support value for pensioner " + supportValue)
-        support = supportValue === "id/10" ? this.supportNumber : ""
         this.textFields.push(
           invalidPensioner[
             (this.fields as PensionerDocuments)[
@@ -181,22 +133,7 @@ class EndText {
       }
     }
 
-    for (let i = 0; i < this.textFields.length; i++) {
-      if (
-        typeof this.textFields[i] === "string" &&
-        this.textFields[i].length === 0
-      )
-        continue
-      if (typeof this.textFields[i] === "undefined") continue
-
-      if (this.textFields[i]) this.midText += `- ${this.textFields[i]};\n`
-    }
-
-    if (this.midText) {
-      return this.upper + "\n" + this.midText + support + this.bottom
-    } else {
-      return "Cadastro completo 👍"
-    }
+    return this.returnText()
   }
 }
 
